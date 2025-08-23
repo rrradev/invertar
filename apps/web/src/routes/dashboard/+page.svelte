476cd0a72e486b03f33901ae5fd359888fb2aca0
radradef <radradef@gmail.com>
2025-08-23 14:39:28 +0300

    clean up

--- apps/web/src/routes/dashboard/+page.svelte
+++ apps/web/src/routes/dashboard/+page.svelte
@@ -643,7 +643,7 @@
 				<!-- Refresh OTAC Action -->
 				<button
 					on:click={() => refreshOTAC(currentAdmin)}
-					disabled={isRefreshing === currentAdmin.id || (currentAdmin.oneTimeAccessCodeExpiry && new Date() <= new Date(currentAdmin.oneTimeAccessCodeExpiry))}
+					disabled={isRefreshing === currentAdmin.id || (currentAdmin.oneTimeAccessCodeExpiry && new Date() <= new Date(currentAdmin.oneTimeAccessCodeExpiry)|| !currentAdmin.hasInitialPassword)}
 					class="group flex items-center w-full px-4 py-2 text-sm {currentAdmin.oneTimeAccessCodeExpiry && new Date() <= new Date(currentAdmin.oneTimeAccessCodeExpiry) ? 'text-gray-400 cursor-not-allowed' : 'text-blue-700 hover:bg-blue-50 hover:text-blue-900'} disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
 					role="menuitem"
 				>
