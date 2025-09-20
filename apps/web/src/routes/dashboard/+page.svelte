55ed3468e5a392095da7519d154d691a678fe961
radradef <radradef@gmail.com>
2025-09-20 20:19:31 +0300

    remove cancel btn

--- apps/web/src/routes/dashboard/+page.svelte
+++ apps/web/src/routes/dashboard/+page.svelte
@@ -1077,15 +1077,6 @@
 							Delete Item
 						{/if}
 					</button>
-
-					<button
-						onclick={closeEditModal}
-						disabled={isUpdatingItem || isDeletingItem}
-						class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
-						data-testid="cancel-edit-button"
-					>
-						Cancel
-					</button>
 				</div>
 			</div>
 		</div>
